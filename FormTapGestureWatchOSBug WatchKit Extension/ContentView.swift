29344f31da92f865c64f89a246d66d786366363e
Jason Ji <jason.ji@capitalone.com>
2019-08-01 12:42:46 -0400

    Initial Commit

---
+++ FormTapGestureWatchOSBug WatchKit Extension/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  FormTapGestureWatchOSBug WatchKit Extension
+//
+//  Created by Jason Ji on 8/1/19.
+//  Copyright © 2019 Jason Ji. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ FormTapGestureWatchOSBug WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  FormTapGestureWatchOSBug WatchKit Extension
+//
+//  Created by Jason Ji on 8/1/19.
+//  Copyright © 2019 Jason Ji. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
